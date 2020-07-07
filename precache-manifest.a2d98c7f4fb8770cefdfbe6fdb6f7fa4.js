0e4e98058b60e949ac51e69348fe226cd45d5997
LP-12 <nirali.patel@green-apex.com>
2020-07-07 18:20:22 +0530

    Updates

---
+++ precache-manifest.a2d98c7f4fb8770cefdfbe6fdb6f7fa4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6efc8ab645533c13118f704bead293b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6da0135147876b4bdace",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a915a71e628f91626f01",
+    "url": "/static/css/main.03b98a79.chunk.css"
+  },
+  {
+    "revision": "6da0135147876b4bdace",
+    "url": "/static/js/2.63eef4eb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.63eef4eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a915a71e628f91626f01",
+    "url": "/static/js/main.bd8a05e8.chunk.js"
+  },
+  {
+    "revision": "246c782504fcf04e5662",
+    "url": "/static/js/runtime-main.042494c0.js"
+  }
+]);
