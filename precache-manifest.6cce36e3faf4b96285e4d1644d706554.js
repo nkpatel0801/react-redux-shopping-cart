5189a3758c579c33b0b92baf011927a61916b350
LP-12 <nirali.patel@green-apex.com>
2020-07-07 18:33:19 +0530

    Updates

---
+++ precache-manifest.6cce36e3faf4b96285e4d1644d706554.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161281ff0e74f805c28430ff814c01af",
+    "url": "/react-redux-shopping-cart/index.html"
+  },
+  {
+    "revision": "6da0135147876b4bdace",
+    "url": "/react-redux-shopping-cart/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cf3d3d5656c7bce3a196",
+    "url": "/react-redux-shopping-cart/static/css/main.03b98a79.chunk.css"
+  },
+  {
+    "revision": "6da0135147876b4bdace",
+    "url": "/react-redux-shopping-cart/static/js/2.63eef4eb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-redux-shopping-cart/static/js/2.63eef4eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf3d3d5656c7bce3a196",
+    "url": "/react-redux-shopping-cart/static/js/main.6b38cc8f.chunk.js"
+  },
+  {
+    "revision": "3c71f214a2f1ca8b8fbc",
+    "url": "/react-redux-shopping-cart/static/js/runtime-main.7eb78899.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a2d98c7f4fb8770cefdfbe6fdb6f7fa4.js"
+  "/react-redux-shopping-cart/precache-manifest.6cce36e3faf4b96285e4d1644d706554.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-redux-shopping-cart/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
